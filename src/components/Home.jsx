61772f1330464b8b8fbc2696efd63e998cc4346f
shoron68 <mr.shoron.7514@gmail.com>
2025-03-20 22:56:21 +0600

    demo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <div className='bg-[#000] w-full h-100vh py-[100px] flex justify-center'>
             <ul className='text-center'>
-                <li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center '><Link to="/faq">FAQ</Link></li>
-                <li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'><Link to="/events">Events</Link></li>
-                <li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'><Link to="/download">Download</Link></li>
-                <li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'><Link to="/navtab">NavTab</Link></li>
-                <li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'><Link to="/slider">Slider</Link></li>
+            <Link to="/faq"> <li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center '>FAQ</li></Link>
+                <Link to="/events"><li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'>Events</li></Link>
+                <Link to="/download"><li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'>Download</li></Link>
+                <Link to="/navtab"><li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'>NavTab</li></Link>
+                <Link to="/slider"><li className='text-[#000] font-bold text-[24px] h-[50px] w-[200px] bg-[#FFCD00] flex justify-center  items-center  my-[20px]'>Slider</li></Link>
             </ul>
     </div>
   )
